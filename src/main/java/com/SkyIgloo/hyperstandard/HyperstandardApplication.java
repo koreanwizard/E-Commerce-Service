291d7da910d46e50f5267b28d07e842ec5fda357
koreanwizard <harry992178@gmail.com>
2025-02-26 21:22:34 +0900

    adding project files

---
+++ src/main/java/com/SkyIgloo/hyperstandard/HyperstandardApplication.java
@@ -0,0 +1,13 @@
+package com.SkyIgloo.hyperstandard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyperstandardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HyperstandardApplication.class, args);
+	}
+
+}
